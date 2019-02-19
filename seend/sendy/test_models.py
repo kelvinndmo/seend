bbce9fdf0d3b6735693d5debdb6c9576a64d5695
JumaKahiga <kabirumwangi@gmail.com>
2019-02-19 11:53:55 +0300

    Make edit

--- seend/sendy/test_models.py
+++ seend/sendy/test_models.py
@@ -4,11 +4,11 @@ from sendy.models import RiderProfile
 # Create your tests here.
 class TestRiderProfileModel(TestCase):
 	def setUp(self):
-		self.rider = RiderProfile(username="Keith Mandela", email="keith@olympians.com", contact_phone="09878", operation_area="Eastern Bypass")
+		self.rider = RiderProfile(username="Keith Mandela", email="keith@olympians.co", contact_phone="09878", operation_area="Eastern Bypass")
 		self.rider.save()
 
 	def test_rider_creation(self):
-		self.assertEqual(self.rider.email, "keith@olympians.com")
+		self.assertEqual(self.rider.email, "keith@olympians.co")
 
 	def test_rider_list(self):
 		self.assertEqual(RiderProfile.objects.count(), 1)
